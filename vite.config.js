478af0efd278498f0ef18b9f174bf19daa96c4dd
oshu <99813212+oshudev@users.noreply.github.com>
2024-12-09 03:10:14 +0800

    fix: Invalid import

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import path from 'path';
-import { fileUrlToPath } from 'url';
+import { fileURLToPath } from 'url';
 
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-const __dirname = path.dirname(fileUrlToPath(import.meta.url));
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
