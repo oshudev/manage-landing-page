f8f67070863bec15b1d44f03c2f6afba84577ef0
oshu <99813212+oshudev@users.noreply.github.com>
2024-12-11 02:44:02 +0800

    feat: Add features section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from '@/sections/header';
-import Hero from './sections/hero';
+import Hero from '@/sections/hero';
+import Features from '@/sections/features';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <Header />
       <main>
         <Hero />
+        <Features />
       </main>
     </>
   );
---
+++ src/sections/features.jsx
@@ -0,0 +1,56 @@
+function Features() {
+  return (
+    <section className="features">
+      <div className="container">
+        <div className="text-container">
+          <h2>What&apos;s different about Manage?</h2>
+          <p>
+            Manage provides all the functionality your team needs, without the
+            complexity. Our software is tailor-made for modern digital product
+            teams.
+          </p>
+        </div>
+        <div className="highlights-container">
+          <ul className="highlight__lists">
+            <li className="highlight__item">
+              <div className="heading">
+                <span className="bullet-point">01</span>
+                <h3>Track company-wide progress</h3>
+              </div>
+              <p>
+                See how your day-to-day tasks fit into the wider vision. Go from
+                tracking progress at the milestone level all the way done to the
+                smallest of details. Never lose sight of the bigger picture
+                again.
+              </p>
+            </li>
+            <li className="highlight__item">
+              <div className="heading">
+                <span className="bullet-point">02</span>
+                <h3>Advanced built-in reports</h3>
+              </div>
+              <p>
+                Set internal delivery estimates and track progress toward
+                company goals. Our customisable dashboard helps you build out
+                the reports you need to keep key stakeholders informed.
+              </p>
+            </li>
+            <li className="highlight__item">
+              <div className="heading">
+                <span className="bullet-point">03</span>
+                <h3>Everything you need in one place</h3>
+              </div>
+              <p>
+                Stop jumping from one service to another to communicate, store
+                files, track tasks and share documents. Manage offers an
+                all-in-one team productivity solution.
+              </p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Features;
