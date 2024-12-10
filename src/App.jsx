0a1f00fa4cf61b3868741e55158901e7e5da3658
oshu <99813212+oshudev@users.noreply.github.com>
2024-12-10 23:34:30 +0800

    feat: Add hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Header from '@/sections/header';
+import Hero from './sections/hero';
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <Hero />
+      </main>
     </>
   );
 }
---
+++ src/sections/hero.jsx
@@ -0,0 +1,31 @@
+import heroIllustration from '@/assets/illustration-intro.svg';
+import pill from '@/assets/bg-tablet-pattern.svg';
+
+function Hero() {
+  return (
+    <section className="hero">
+      <div className="pill">
+        <img src={pill} alt="" />
+      </div>
+      <div className="container">
+        <div className="img-container">
+          <img src={heroIllustration} alt="" />
+        </div>
+        <div className="content-container">
+          <div className="text-container">
+            <h1>Bring everyone together to build better products.</h1>
+            <p>
+              Manage makes it simple for software teams to plan day-to-day tasks
+              while keeping the larger team goals in view.
+            </p>
+          </div>
+          <a href="#" className="button button--primary">
+            Get Started
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
